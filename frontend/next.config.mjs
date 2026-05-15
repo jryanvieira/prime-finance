10a8bda0307aaedf2fdaa55f410009e243904b52
João Araujo <jryanvieira10@gmail.com>
2026-05-15 13:50:56 -0300

    feat(ZEM-43): instalar next-pwa e configurar service worker (#23)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
+import withPWAInit from '@ducanh2912/next-pwa'
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -8,4 +20,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPWA(nextConfig)
